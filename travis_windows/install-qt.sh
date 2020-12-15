9f032f82147f27bbcff198c5514a837003add73d
Martin Lopatar <lop_in_github@dataplex.cz>
2020-12-15 18:43:22 +0100

    now33

--- travis_windows/install-qt.sh
+++ travis_windows/install-qt.sh
@@ -7,6 +7,7 @@ set -ev
 # QT_INSTALL_MSVC_PACKAGE (e.g. 'qt.qt5.599.win64_msvc2015_64' )
 
 ### optional "input" env. variables:
+# QT_INSTALL_LOG
 # QT_INSTALL_ACCOUNT_LOGIN
 # QT_INSTALL_ACCOUNT_PASSWORD
 
@@ -14,4 +15,4 @@ echo '### installing Qt prerequisites using Qt online installer'
 mkdir -p "$(dirname "$QT_INSTALLER_PATH")"
 export QT_INSTALL_PACKAGES="$QT_INSTALL_MSVC_PACKAGE"
 cd "$(dirname "$(realpath "$0")")"
-"$(realpath "$QT_INSTALLER_PATH")" -v --script 'install-qt.qs'
+"$(realpath "$QT_INSTALLER_PATH")" -v --script 'install-qt.qs' &> "${QT_INSTALL_LOG:-/dev/stdout}"
