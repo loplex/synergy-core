4e51c31bd961f3604550d7f6ec88f773aa571659
Martin Lopatar <lop_in_github@dataplex.cz>
2020-12-16 04:19:07 +0100

    now45

--- travis_windows/install-wix.sh
+++ travis_windows/install-wix.sh
@@ -6,4 +6,5 @@ echo '### installing WiX prerequisite'
 #nuget install WiX.Toolset
 #choco install wixtoolset
 powershell Install-WindowsFeature Net-Framework-Core
-choco install wixtoolset --version 3.11.1
+#choco install wixtoolset --version 3.11.1
+cinst -y wixtoolset
