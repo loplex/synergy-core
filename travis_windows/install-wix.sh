f80cc94674a7fd8eae8cf60a3bef2f3f1560e3aa
Martin Lopatar <lop_in_github@dataplex.cz>
2020-12-16 06:08:55 +0100

    now52

--- travis_windows/install-wix.sh
+++ travis_windows/install-wix.sh
@@ -10,4 +10,6 @@ powershell Install-WindowsFeature Net-Framework-Core
 #choco install wixtoolset --version 3.11.1
 #choco install microsoft-build-tools
 #choco install visualstudio2015buildtools
+choco install microsoft-build-tools --version=14.0.25420.1
+which -a cinst
 cinst -y wixtoolset
