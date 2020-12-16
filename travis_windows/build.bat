812755e14bae242e7d550ebe491fab6bccabccbd
Martin Lopatar <lop_in_github@dataplex.cz>
2020-12-16 17:07:39 +0100

    test62test3

--- travis_windows/build.bat
+++ travis_windows/build.bat
@@ -1,4 +1,5 @@
 echo on
 cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Debug ..
-"/c/Program Files (x86)/MSBuild/14.0/Bin/amd64/MSBuild.exe" synergy-core.sln /p:Platform="x64" /p:Configuration=Debug /m /verbosity:diag
-"/c/Program Files (x86)/MSBuild/14.0/Bin/amd64/MSBuild.exe" synergy-core.sln /p:Platform="x86" /p:Configuration=Debug /m /verbosity:diag
+echo AHOJ
+"C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe" "synergy-core.sln" /verbosity:diag
+echo AHOJKY
