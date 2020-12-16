84511054080104be48b7c4b3561921e52267ef42
Martin Lopatar <lop_in_github@dataplex.cz>
2020-12-16 19:38:27 +0100

    revert test commits

--- travis_windows/build.bat
+++
@@ -1,6 +0,0 @@
-echo HUUU
-echo on
-cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Debug ..
-echo AHOJ
-"C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe" "synergy-core.sln" /verbosity:diag
-echo AHOJKY
