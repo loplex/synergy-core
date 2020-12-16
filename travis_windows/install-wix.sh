46b71c302e400108f678863c4923c382c81dffbf
Martin Lopatar <lop_in_github@dataplex.cz>
2020-12-16 05:27:49 +0100

    now49

--- travis_windows/install-wix.sh
+++ travis_windows/install-wix.sh
@@ -5,8 +5,8 @@ echo '### installing WiX prerequisite'
 #choco install nuget.commandline
 #nuget install WiX.Toolset
 #choco install wixtoolset
-#powershell Install-WindowsFeature Net-Framework-Core
-powershell Install-WindowsFeature Net-Framework
+powershell Install-WindowsFeature Net-Framework-Core
+#powershell Install-WindowsFeature Net-Framework
 #choco install wixtoolset --version 3.11.1
 #choco install microsoft-build-tools
 #choco install visualstudio2015buildtools
